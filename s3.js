359ef4e97ab916da2fff90c7af0af7d5b1d338af
AnthonyCalandra <AnthonyCalandra@172.22.11.149>
2014-07-02 18:45:55 -0400

    Support for user-defined functions.

--- s3.js
+++ s3.js
@@ -211,6 +211,10 @@
                     return percentValue;
                 }
             };
+            for (var fn in settings.functions) {
+                functions[fn] = settings.functions[fn];
+            }
+            
             return functions;
         })();
     
@@ -259,11 +263,12 @@
     
     // TODO: store elem globally to prevent re-creating element for every calculation?
     function calc(expr) {
-        var elem = document.createElement("div");
+        var elem = document.createElement("div"),
+            val = "";
         elem.style.display = "none";
         elem.style.top = "calc(" + expr + ")";
         document.body.appendChild(elem);
-        var val = window.getComputedStyle(elem, null).getPropertyValue("top");
+        val = window.getComputedStyle(elem, null).getPropertyValue("top");
         document.body.removeChild(elem);
         return val;
     }
